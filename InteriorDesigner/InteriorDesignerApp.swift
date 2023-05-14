abeaebdabae461d067f57e070ea0beb6e04cf6e5
wee <87364144+weex1997@users.noreply.github.com>
2023-05-14 12:02:10 +0300

    Initial Commit

---
+++ InteriorDesigner/InteriorDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteriorDesignerApp.swift
+//  InteriorDesigner
+//
+//  Created by Wedad Almehmadi on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct InteriorDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
