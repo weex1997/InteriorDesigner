0d3fc771234e10ca0da8a68943bd5c9549bc2541
wee <87364144+weex1997@users.noreply.github.com>
2023-06-01 12:34:58 +0300

    Revert "Merge branch 'BackendWedad'"
    This reverts commit 599e37e12c9a8bcb2d424aeef940e350f91f4718, reversing
    changes made to c8b818aa267b64ad4db11531e95cdafd85e30373.

--- InteriorDesigner/InteriorDesignerApp.swift
+++ InteriorDesigner/InteriorDesignerApp.swift
@@ -6,16 +6,12 @@
 //
 
 import SwiftUI
-import Firebase
 
 @main
 struct InteriorDesignerApp: App {
-    init() {
-            // NOTE: You need to add your own GoogleService-Info.plist to this Xcode project so that it points to your Firestore database instance
-        FirebaseApp.configure()        }
     var body: some Scene {
         WindowGroup {
-            SignInButton()
+            ContentView()
         }
     }
 }
