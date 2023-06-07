73557517b558abc676cf03c5d7104e5cbd67b8ae
nhajarn <44345143+nhajarn@users.noreply.github.com>
2023-06-07 10:58:35 +0300

    editKey

--- InteriorDesigner/Constants.swift
+++ InteriorDesigner/Constants.swift
@@ -7,10 +7,12 @@
 import Foundation
 
 enum Constants {
-    static let openAIApiKey = "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"
+    static let openAIApiKey = "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"
+    
     //#Alternatine
     //
-//    static let openAIApiKey = "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"
-
-
+    //"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"
+    //    "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"
+    
+    
 }
